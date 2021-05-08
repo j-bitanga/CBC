e766897e8735c333a0921b445cc8b83d717d3bdc
j-bitanga <jbitanga88@gmail.com>
2021-05-07 17:00:35 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
-
+import Navbar from './Components/Navbar/Navbar';
+import "../src/Components/App.css"
 const App = () => {
 
+return(
+    <div className="App">
+        <Navbar />
+    </div>
+)
 
 
 }
---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,25 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url:'Link goes here',
+        cName: 'nav-links',
+    },
+    {
+        title:'Services',
+        url:'Link goes here',
+        cName: 'nav-links',
+    },
+
+    {
+        title:'Contact Us',
+        url:'Link goes here',
+        cName: 'nav-links',
+    },
+    {
+        title:'Sign Up',
+        url:'Link goes here',
+        cName: 'nav-links-mobile',
+    },
+]
+
+export default MenuItems
